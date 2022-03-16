12157ec2ab61028ce6b96aff2a8cf3f37b7c86a7
sqreele <kkanthong@gmail.com>
2022-03-16 22:28:53 +0700

    make it better5

--- BlogApi/settings.py
+++ BlogApi/settings.py
@@ -137,7 +137,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_URL = os.path.join(BASE_DIR,'static')
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
