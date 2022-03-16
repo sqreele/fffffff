07ce246e70582153559ce205c6a962b841d670db
sqreele <kkanthong@gmail.com>
2022-03-16 22:05:17 +0700

    make it better3

--- BlogApi/settings.py
+++ BlogApi/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-p&w^gb5vmbhwgxbv)^%v10(=zd!(!x_ywhc!t90q$hcbp$-z*2'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
